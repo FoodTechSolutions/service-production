ab7ad6d26a5d47b5e9c3ab8a68968df08f63496e
Leonardo Zanaro <leonardo.zanaro@icloud.com>
2024-07-13 14:13:00 -0300

    add services on program

--- ServiceProduction/WebApi/Program.cs
+++ ServiceProduction/WebApi/Program.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Application.Services;
+using Domain.Repositories;
+using Domain.Services;
+using Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductionService, ProductionService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
+builder.Services.AddScoped<IProductIngredientRepository, ProductIngredientRepository>();
+builder.Services.AddScoped<IProductionRepository, ProductionRepository>();
+builder.Services.AddScoped<IProductionProductRepository, ProductionProductRepository>();
 
 var app = builder.Build();
 
