c8255d0f6385840da1b24658f4586f72a54b9670
Marlon <102394517+MarlonMRibeiro@users.noreply.github.com>
2024-09-08 14:28:12 -0300

    Adding context configuration

--- ServiceProduction/WebApi/Program.cs
+++ ServiceProduction/WebApi/Program.cs
@@ -1,7 +1,10 @@
 using Application.Services;
 using Domain.Repositories;
 using Domain.Services;
+using Infrastructure.Context;
 using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Registrar o ProductionContext
+builder.Services.AddDbContext<ProductionContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
+builder.Services.AddScoped<IProductIngredientRepository, ProductIngredientRepository>();
+builder.Services.AddScoped<IProductionProductRepository, ProductionProductRepository>();
+builder.Services.AddScoped<IProductionRepository, ProductionRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+
 
 builder.Services.AddScoped<IProductionService, ProductionService>();
 builder.Services.AddScoped<IProductService, ProductService>();
